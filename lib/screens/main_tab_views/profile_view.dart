7638590e9b9350f463d7d547d3c7087cc45b6370
Hamza Hawkins <4063500+hhawkins@users.noreply.github.com>
2021-03-01 21:10:22 -0500

    Major changes ready for preview on Monday

--- lib/screens/main_tab_views/profile_view.dart
+++ lib/screens/main_tab_views/profile_view.dart
@@ -1,3 +1,6 @@
+import 'package:Date_Roulette/components/rounded_button.dart';
+import 'package:Date_Roulette/constants.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import '../welcome_screen.dart';
@@ -13,17 +16,112 @@ class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: null,
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.close),
-                onPressed: () {
-                  Navigator.pushNamed(context, WelcomeScreen.id);
-                }),
-          ],
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: Scaffold());
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0), 
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Hero(
+                      tag: 'JJ',
+                      child: Container(
+                        child: Image.asset('images/JJ.png'),
+                        width: 370.0,
+                      ),
+                    ),
+                    // Container(
+                    // margin: EdgeInsets.all(2.0),
+                    // decoration: BoxDecoration(
+                    //   color: kColorBlack,
+                    //   shape: BoxShape.circle
+                    // ),),
+                    // Text(
+                    //   'John Jacobs'
+                    // ),
+                  ],
+                ),
+                SizedBox(
+                  height: 60.0,
+                ),
+                RoundedButton(
+                  title: 'Profile',
+                    color: kColorWhite,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                RoundedButton(
+                  title: 'Preferences',
+                    color: kColorWhite,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                RoundedButton(
+                  title: 'Notifications',
+                    color: kColorWhite,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                RoundedButton(
+                  title: 'Invite Friends',
+                    color: kColorWhite,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                SizedBox(
+                  height: 70.0,
+                ),
+                RoundedButton(
+                  title: 'Save',
+                    color: kButtonPrimaryColor,
+                    textColor: kColorWhite,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  
+                  children: <Widget>[
+                    RoundedButton(
+                  title: 'Delete Account',
+                    color: kColorWhite,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                RoundedButton(
+                  title: 'Log Out',
+                    color: kButtonSecondaryColor,
+                    textColor: kColorBlack,
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).pop();
+                    },
+                ),
+                  ]
+                ),
+              ],)
+            ]
+          ),
+        )
+    );
   }
 }
